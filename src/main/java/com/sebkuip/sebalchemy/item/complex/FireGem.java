3cbc7f9f0f06c9601fb96e1c0ee22bdb3022c4f0
sebkuip <kuipers.sebastian@ziggo.nl>
2022-03-18 17:36:16 +0100

    Now with animation!

--- src/main/java/com/sebkuip/sebalchemy/item/complex/FireGem.java
+++ src/main/java/com/sebkuip/sebalchemy/item/complex/FireGem.java
@@ -1,6 +1,7 @@
 package com.sebkuip.sebalchemy.item.complex;
 
 import com.sebkuip.sebalchemy.util.ModSoundEvents;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
@@ -12,42 +13,113 @@ import net.minecraft.particles.BlockParticleData;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class FireGem extends Item {
     public FireGem(Properties properties) {
         super(properties);
+        MinecraftForge.EVENT_BUS.addListener(FireGem::serverTick);
+    }
+
+    private static final List<ConvertingBlock> converting = new ArrayList<>();
+
+    private boolean addConverting(ConvertingBlock block) {
+        if (converting.contains(block)) {
+            return false;
+        } else {
+            converting.add(block);
+            return true;
+        }
+    }
+
+    private static void serverTick(TickEvent.ServerTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) return;
+
+        List<ConvertingBlock> toRemove = new ArrayList<>();
+        for (ConvertingBlock block: converting) {
+            if (block.frame == 30) {
+                toRemove.add(block);
+                block.lastFrame();
+            } else {
+                block.playFrame();
+                block.frame++;
+            }
+        }
+        for (ConvertingBlock block: toRemove) {
+            converting.remove(block);
+        }
     }
 
 
     @Override
     public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
+        double chance = 0.35;
         World world = Objects.requireNonNull(context.getPlayer()).getCommandSenderWorld();
 
         if(!world.isClientSide()) {
             PlayerEntity playerEntity = context.getPlayer();
             BlockState clickedBlock = world.getBlockState(context.getClickedPos());
-
+            ServerWorld serverWorld = (ServerWorld) world;
             if(stack.getDamageValue() == stack.getMaxDamage()-1) {
                 return ActionResultType.FAIL;
             }
 
             if (clickedBlock.getBlock() == Blocks.STONE) {
-                world.setBlock(context.getClickedPos(), Blocks.GOLD_BLOCK.defaultBlockState(), 3);
-                world.playSound(null, context.getClickedPos(), ModSoundEvents.STONE_TO_GOLD.get(), SoundCategory.BLOCKS, 1, 1);
-                for(int i=0; i<10; i++) {
-                    System.out.println("Particle");
-                    world.addParticle(new BlockParticleData(ParticleTypes.BLOCK, Blocks.GOLD_BLOCK.defaultBlockState()),
-                            context.getClickedPos().getX() + random.nextDouble(), context.getClickedPos().getY() + 0.50, context.getClickedPos().getZ() + random.nextDouble(),
-                            0d, 0d, 0d);
+                ConvertingBlock block = new ConvertingBlock(chance > random.nextDouble(), serverWorld, context.getClickedPos());
+                if (addConverting(block)) {
+                    stack.hurtAndBreak(1, playerEntity, player -> player.broadcastBreakEvent(EquipmentSlotType.MAINHAND));
+                    serverWorld.playSound(null, context.getClickedPos(), ModSoundEvents.STONE_TO_GOLD.get(), SoundCategory.BLOCKS, 1, 1);
+                } else {
+                    return ActionResultType.FAIL;
                 }
-                stack.hurtAndBreak(1, playerEntity, player -> player.broadcastBreakEvent(EquipmentSlotType.MAINHAND));
             }
         }
-
         return ActionResultType.SUCCESS;
     }
+}
 
+class ConvertingBlock {
+    public int frame;
+    boolean success;
+    ServerWorld world;
+    BlockPos pos;
+    public ConvertingBlock(boolean success, ServerWorld world, BlockPos pos) {
+        frame = 0;
+        this.success = success;
+        this.world = world;
+        this.pos = pos;
+    }
+
+    public void lastFrame() {
+        if (success) {
+            world.sendParticles(new BlockParticleData(ParticleTypes.BLOCK, Blocks.GOLD_BLOCK.defaultBlockState()),
+                    pos.getX() + 0.5d, pos.getY() + 0.5d, pos.getZ() + 0.5d,
+                    10, 0.1d, 0.1d, 0.1d, 2d);
+            world.setBlock(pos, Blocks.GOLD_BLOCK.defaultBlockState(), 3);
+        } else {
+            world.sendParticles(new BlockParticleData(ParticleTypes.BLOCK, Blocks.REDSTONE_BLOCK.defaultBlockState()),
+                    pos.getX() + 0.5d, pos.getY() + 0.5d, pos.getZ() + 0.5d,
+                    10, 0.1d, 0.1d, 0.1d, 2d);
+        }
+    }
+
+    public void playFrame() {
+        if (frame % 5 == 0) {
+            world.sendParticles(new BlockParticleData(ParticleTypes.BLOCK, Blocks.STONE.defaultBlockState()),
+                    pos.getX() + 0.5d, pos.getY() + 0.5d, pos.getZ() + 0.5,
+                    3, 0.1d, 0.1d, 0.1d, 2d);
+        }
+    }
+
+    public boolean equals(ConvertingBlock other) {
+        return this.pos == other.pos;
+    }
 }
